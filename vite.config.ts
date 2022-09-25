64baf32dfe8270c4fbb3f854e9a6d7d82f276c30
openriceworks <openriceworks@gmail.com>
2022-09-25 16:19:53 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { build, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/api-viewer/',
   build: {
     outDir: "build"
   }
